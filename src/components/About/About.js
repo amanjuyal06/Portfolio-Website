1cdd7fbb82405b22c73ca733e45a23a5180bdff7
Aman <amanjuyalofficial@gmail.com>
2024-09-13 15:40:34 +0530

    Initial commit with my changes

---
+++ src/components/About/About.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { stackList } from "../../data/ProjectData";
+import {
+  Image,
+  Technologies,
+  Tech,
+  TechImg,
+  TechName,
+  ContactWrapper,
+} from "./AboutElements";
+import ScrollAnimation from "react-animate-on-scroll";
+function About() {
+  return (
+    <ContactWrapper id="about">
+      <div className="Container">
+        <div className="SectionTitle">About Me</div>
+        <div className="BigCard">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <Image
+            src="/man-svgrepo-com.svg"
+            alt="man-svgrepo"
+          />
+        </ScrollAnimation>
+          <div className="AboutBio">
+            <ScrollAnimation animateIn="fadeInLeft">
+            Hello! My name is <strong>Aman Juyal</strong>.
+            </ScrollAnimation>
+
+            <br /><br />
+            
+            <ScrollAnimation animateIn="fadeInLeft">
+            I am currently studying in final year of 4 year Btech CSE Program at Graphic Era Deemed to be University in Dehradun. I am a passionate software engineer with a strong focus on developing innovative solutions that combine creativity and technical expertise. 
+            </ScrollAnimation>
+
+            <br /><br />
+
+            <ScrollAnimation animateIn="fadeInLeft">
+              Working collaboratively within a team towards a shared objective has been an incredibly rewarding and unique experience for me. I am eager to continue exploring exciting projects in the future, with a particular interest in web development and cross-platform mobile development.
+              <div className="tagline2">
+                I have become confident using the following technologies:
+              </div>
+            </ScrollAnimation>
+            
+
+            <Technologies>
+              {stackList.map((stack, index) => (
+                <ScrollAnimation animateIn="fadeInLeft" key={index}>
+                  <Tech key={index} className="tech">
+                    <TechImg src={stack.img} alt={stack.name} />
+                    <TechName>{stack.name}</TechName>
+                  </Tech>
+                </ScrollAnimation>
+              ))}
+            </Technologies>
+          </div>
+
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default About;
