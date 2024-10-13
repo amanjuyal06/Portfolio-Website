2847b66240697d6e8d0c3ad4a6df4660c6991c91
Aman <amanjuyalofficial@gmail.com>
2024-10-13 11:39:26 +0530

    Updated Resume

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,7 +25,7 @@ const Header = ({ toggle }) => {
         <NavBtn>
           <a
             className="btn PrimaryBtn"
-            href="https://drive.google.com/file/d/1-8Zc2EMyZWIUsXskxh-vqH5MN2krFmC1/view?usp=sharing"
+            href="https://drive.google.com/file/d/1-2Wx2JoWUElTHRE4kKFag2eTOGo-93XR/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
